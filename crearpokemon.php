3dc4aa3384fc30de7392704d0e556d81114a2a56
dsanchezg-campus <dsanchezg@campusdigitalfp.com>
2026-03-16 11:26:43 +0100

    enseñar movimientos de la entrenadora

--- crearpokemon.php
+++ crearpokemon.php
@@ -13,45 +13,59 @@ session_start();
 </head>
 <body>
     <section>
+        <a href="gestion.php">Ir a gestión</a>
+        <a href="crearentrenadora.php">Ir a entrenadoras</a>
+    </section>
+    <section>
         <h1>Crear Pokemon</h1>
         <form method="POST" action="">
-        <label for="nombre"><p>Nombre</p></label>
-        <input type="text" name="nombre">
-        <label for="elemento"><p>Elemento</p></label>
-        <input type="text" name="elemento">
-        <label for="tipo"><p>Tipo</p></label>
-        <input type="text" name="tipo">
-        <label for="ataque"><p>Ataque</p></label>
-        <input type="text" name="ataque"><p></p>
-        <input type="submit" value="crear">
+            <label for="nombre"><p>Nombre</p></label>
+            <input type="text" name="nombre">
+            <label for="elemento"><p>Elemento</p></label>
+            <input type="text" name="elemento">
+            <label for="tipo"><p>Tipo</p></label>
+            <input type="text" name="tipo">
+            <label for="movimiento"><p>Movimiento</p></label>
+            <input type="text" name="movimiento" id="movimiento">
+            <label for="dano"><p>Daño</p></label>
+            <input type="number" name="dano" id="dano">
+            <label for="precision"><p>Precision</p></label>
+            <input type="number" name="precision" id="precision" min="10" max="100">
+            <label for="usos"><p>Usos</p> </label>
+            <input type="number" name="usos" id="usos" min="5" max="30">
+            <p></p>
+            <input type="submit" value="crear">
         </form>
 
     </section>
 
-    <section>
     <?php
     if(!isset($_SESSION["pokemons"])){
         $_SESSION["pokemons"]=[];
     }
 
-    if(isset($_POST["nombre"], ($_POST["elemento"]),($_POST["tipo"]),($_POST["ataque"]))){
+    if(isset($_POST["nombre"], $_POST["elemento"], $_POST["tipo"], $_POST["movimiento"], $_POST["dano"], $_POST["precision"], $_POST["usos"])){
         $nombre = $_POST["nombre"];
         $elemento = $_POST["elemento"];
         $tipo = $_POST["tipo"];
-        $ataque = $_POST["ataque"];
+        $movimiento = $_POST["movimiento"];
+        $dano = $_POST["dano"];
+        $precision = $_POST["precision"];
+        $usos = $_POST["usos"];
 
-        $pokemon = new Pokemon($nombre, $elemento, $tipo, $ataque);
+        $pokemon = new Pokemon($nombre, $elemento, $tipo, $movimiento, $dano, $precision, $usos);
         $_SESSION["pokemons"][]=$pokemon;
 
         $mensaje= "Pokemon creado con éxito.";
     }
 
     if (isset($mensaje)){
+        echo "<section>";
         echo "<p>$mensaje</p>";
+        echo "</section>";
     }  
 
     ?>
-    </section>
 
     <section>
         <h2>Pokémon creados</h2>
@@ -65,9 +79,6 @@ session_start();
         }
         ?>
     </section>
-    <section>
-    <a href="gestion.php">Ir a gestión</a>
-    <a href="crearentrenadora.php">Ir a entrenadoras</a>
-    </section>
+
 </body>
 </html>
